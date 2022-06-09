c38e17e580086a93c531274143b15d5bcdc4cfbf
marsvillager <u201814727@hust.edu.cn>
2022-06-09 15:09:42 +0800

    fix: multiple port

--- test/test_forward.cc
+++ test/test_forward.cc
@@ -6,17 +6,24 @@ using namespace faker_tsn;
 using namespace std;
 
 static void Sender() {
-    // crate port 1
     ConfigSetting& cs = ConfigSetting::getInstance();
     /* load config file */
-    const char* deviceName1 = cs.get<const char*>("deviceName");
-    shared_ptr<IPort> port1 = make_shared<DataPort>(deviceName1);
+    const char* deviceName = cs.get<const char*>("deviceName");
+
+    // crate port 1
+    shared_ptr<IPort> port1 = make_shared<DataPort>(deviceName);
     shared_ptr<IPortState> creationState1 = make_shared<CreationPortState>();
     creationState1->doAction(port1);
+
+    // crate port 2
+    shared_ptr<IPort> port2 = make_shared<DataPort>(deviceName);
+    shared_ptr<IPortState> creationState2 = make_shared<CreationPortState>();
+    creationState1->doAction(port2);
 
     // add port into port manager
     shared_ptr<PortManager> portManager = TSNContext::getInstance().getPortManager();
     portManager->appendPort(port1);
+    portManager->appendPort(port2);
 
     /* load config file */
     string filename = cs.get<string>("routesDir");
